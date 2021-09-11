cdfda5ed956f718edc313f7ab7c07257fd4f61ae
siri <maddakasireesha@gmail.com>
2021-09-11 17:00:19 +0530

    second commit

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import React from 'react';
+import './Header.css';
+export default class Header extends Component{
+    render(){
+        return(
+            <div className='header'>
+                <img src='https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg'></img>
+            </div>
+        );
+    }
+}
