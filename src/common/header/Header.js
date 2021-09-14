9557876d276685df0587f5d16d270b2f73d4810d
siri <maddakasireesha@gmail.com>
2021-09-14 22:01:21 +0530

    third commit

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component{
     render(){
         return(
             <div className='header'>
-                <img src='https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg'></img>
+                <img id='logo' src='https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg'></img>
             </div>
         );
     }
---
+++ src/screens/home/Home.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import Header from '../../common/header/Header';
+import './Home.css'
+import GridListBanner from "../../common/GridListBanner";
+import Movielistdisplay from "../../common/MovieListDisplay";
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div><Header/>
+                <div className='head'>
+                    <span>Upcoming Movies</span>
+                </div>
+                <GridListBanner />
+                <div className="flex-container">
+                    <div className="left">
+                        <Movielistdisplay />
+                    </div>
+                    <div className="right">
+
+                    </div>
+                </div>
+            
+            </div>
+        );
+    }
+}
